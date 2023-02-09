5cbeefe6219c131f67d8caf101cf5636852f7fad
Reema Al-Marzoog <reemaa@google.com>
2023-02-09 13:50:06 -0500

    rebase

--- server/conf/evolutions/default/47.sql
+++ server/conf/evolutions/default/47.sql
@@ -1,15 +1,7 @@
 # --- !Ups
 
-<<<<<<< HEAD
-alter table applications add column if not exists submitted_by_trusted_intermediary boolean;
-
-# --- !Downs
-
-alter table applications drop column if exists submitted_by_trusted_intermediary;
-=======
 alter table programs add column program_type varchar default 'default';
 
 # --- !Downs
 
 alter table programs drop column if exists program_type;
->>>>>>> 9b3bf93b (Add a new ProgramType enum to Programs. (#4187))
